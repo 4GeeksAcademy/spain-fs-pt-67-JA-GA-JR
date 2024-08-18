729fd9a197638ca1bae2b3fd847d6dd832282546
8u1137pr00f <162195925+8u1137pr00f@users.noreply.github.com>
2024-08-18 21:10:18 +0000

    actualizaciones CSS

--- src/front/js/pages/registroUsuario.js
+++ src/front/js/pages/registroUsuario.js
@@ -1,8 +1,6 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from 'react-router-dom';
-
 import { Context } from "../store/appContext";
-
 import "../../styles/registroUsuarios.css";
 import { jsx } from "react/jsx-runtime";
 export const Registro = () => {
